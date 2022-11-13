00caeaa945f9446bd384a1be299ad88de16145d8
Maxym Lyskov <raypember02@gmail.com>
2022-11-13 21:01:31 +0200

    Prod changes.

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -6,7 +6,7 @@ function MainTitle() {
       <h2 data-aos="fade-down">I enjoy working with your ideas</h2>
       <h3 data-aos="fade-up">
         You come up with something - I do it in the better possible way. React,
-        React Native, Node JS, check out what you are intersting on the{" "}
+        React Native, Node JS, check out what you are intersted on the{" "}
         <Link to="/about">about page</Link>
       </h3>
     </div>
